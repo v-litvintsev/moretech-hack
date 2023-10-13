e331a2403b5c5ef75241ccdcf40a4b2b60303742
aleksandrkisler <aka.kisler@gmail.com>
2023-10-14 02:03:40 +0300

    remove trash

--- front-app/layouts/default.vue
+++ front-app/layouts/default.vue
@@ -27,9 +27,6 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-
-    </v-navigation-drawer>
   </v-app>
 </template>
 
@@ -38,25 +35,13 @@ export default {
   name: 'DefaultLayout',
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
           icon: 'mdi-apps',
           title: 'Welcome',
           to: '/',
         },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
     }
   },
 }
